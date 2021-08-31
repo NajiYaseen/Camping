387250b352ca7f96b2f63c3fd0ce34a96b7cafe1
NajiYaseen <naji.yaseen.ps@gmail.com>
2021-08-31 21:55:46 +0300

    fixing errors

--- app.js
+++ app.js
@@ -73,6 +73,29 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+
 app.use(passport.initialize());
 app.use(passport.session());
 
